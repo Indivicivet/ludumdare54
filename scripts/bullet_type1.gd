0e4413608000a5a6a8aee852891c4e1b1dc37704
indi <indivicivet@gmail.com>
2023-09-30 23:08:52 +0100

    simplify bullet 1 logic

--- scripts/bullet_type1.gd
+++ scripts/bullet_type1.gd
@@ -12,10 +12,11 @@ func _physics_process(delta):
 
 
 func _on_body_entered(body):
+	if body.name == "player":
+		return
 	if body.has_method("got_shot"):
 		body.got_shot(self)
-	if not body.name == "player":
-		var hit_indicator_obj = hit_indicator.instantiate()
-		get_tree().get_root().add_child(hit_indicator_obj)
-		hit_indicator_obj.transform = self.global_transform
-		queue_free()
+	var hit_indicator_obj = hit_indicator.instantiate()
+	get_tree().get_root().add_child(hit_indicator_obj)
+	hit_indicator_obj.transform = self.global_transform
+	queue_free()
