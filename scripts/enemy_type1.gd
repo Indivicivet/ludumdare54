9f65e72bd0f04900c3d9fc46c754a77569826682
indi <indivicivet@gmail.com>
2023-09-30 22:29:06 +0100

    make enemies shootable/killable

--- scripts/enemy_type1.gd
+++ scripts/enemy_type1.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 const SPEED = 100
 
 var target_pos = Vector2(0, 0)
+var hp = 1
 
 
 @onready var navigation_agent = get_node("NavigationAgent2D")
+@onready var sprite_obj = get_node("Sprite2D")
 @onready var player = get_tree().get_root().get_node("game_root").get_node("player")
 
 
@@ -14,6 +16,13 @@ func _ready():
 	navigation_agent.path_desired_distance = 1.0
 	navigation_agent.target_desired_distance = 1.0
 	call_deferred("pathfinding_setup")
+
+
+func got_shot(other):
+	hp -= 0.02
+	sprite_obj.modulate = Color(hp / 2, hp / 2, hp / 2)
+	if hp < 0:
+		queue_free()
 
 
 func pathfinding_setup():
