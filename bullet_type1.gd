c439c56f303e161d50674ed5bdd2cbbbd3eaec49
indi <indivicivet@gmail.com>
2023-09-30 21:07:07 +0100

    fire bullets towards the mouse

--- bullet_type1.gd
+++ bullet_type1.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
 
-const SPEED = 500
-const DIR_COS = 1
-const DIR_SIN = 0
+const SPEED = 500.0
+var dir_cos = 1.0
+var dir_sin = 0.0
 
 
 func _physics_process(delta):
-	position.x += SPEED * DIR_COS * delta
-	position.y += SPEED * DIR_SIN * delta
+	position.x += SPEED * dir_cos * delta
+	position.y += SPEED * dir_sin * delta
 
 
 func _on_body_entered(body):
