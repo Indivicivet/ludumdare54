c6b369d49b855f794177632467dd5dc9c341dd56
indi <indivicivet@gmail.com>
2023-09-30 21:17:26 +0100

    make firing optional

--- scripts/gun1.gd
+++ scripts/gun1.gd
@@ -5,13 +5,13 @@ var bullet_type_1 = preload("res://bullet_type_1.tscn")
 
 
 func _process(delta):
-	#if Input.is_action_pressed("primary_fire"):
-	var bullet_inst = bullet_type_1.instantiate()
-	owner.add_child(bullet_inst)
-	bullet_inst.transform = self.global_transform
-	var mouse_pos = get_viewport().get_mouse_position()
-	var dir_x = mouse_pos.x - self.global_transform.get_origin().x
-	var dir_y = mouse_pos.y - self.global_transform.get_origin().y
-	var dir_r = (dir_x ** 2 + dir_y ** 2) ** 0.5
-	bullet_inst.dir_cos = dir_x / dir_r
-	bullet_inst.dir_sin = dir_y / dir_r
+	if Input.is_action_pressed("primary_fire"):
+		var bullet_inst = bullet_type_1.instantiate()
+		owner.add_child(bullet_inst)
+		bullet_inst.transform = self.global_transform
+		var mouse_pos = get_viewport().get_mouse_position()
+		var dir_x = mouse_pos.x - self.global_transform.get_origin().x
+		var dir_y = mouse_pos.y - self.global_transform.get_origin().y
+		var dir_r = (dir_x ** 2 + dir_y ** 2) ** 0.5
+		bullet_inst.dir_cos = dir_x / dir_r
+		bullet_inst.dir_sin = dir_y / dir_r
