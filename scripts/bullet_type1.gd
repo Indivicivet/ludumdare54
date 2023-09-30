4e25bc05e60fe0d1f75d9f98d86c125d3a65d6cf
indi <indivicivet@gmail.com>
2023-09-30 22:52:48 +0100

    separate out sound playing to a new hit indicator scene (so multiple can play at once)

--- scripts/bullet_type1.gd
+++ scripts/bullet_type1.gd
@@ -1,10 +1,7 @@
 extends Area2D
 
 
-var hitsound = preload("res://sfx/hitHurt0001.wav")
-
-@onready var audiostream_obj = get_tree().get_root().get_node("game_root").get_node("AudioStreamPlayer2D")
-
+var hit_indicator = preload("res://hit_indicator_0001.tscn")
 
 const SPEED = 500.0
 var dir = Vector2(1, 0)
@@ -16,8 +13,9 @@ func _physics_process(delta):
 
 func _on_body_entered(body):
 	if body.has_method("got_shot"):
-		audiostream_obj.stream = hitsound
-		audiostream_obj.play()
 		body.got_shot(self)
 	if not body.name == "player":
+		var hit_indicator_obj = hit_indicator.instantiate()
+		get_tree().get_root().add_child(hit_indicator_obj)
+		hit_indicator_obj.transform = self.global_transform
 		queue_free()
