8a4f1a9a6fe63781eb62f34c3b23a6513b76a5b0
indi <indivicivet@gmail.com>
2023-09-30 23:13:11 +0100

    add scatter to where hit indicator is created

--- hit_indicator_0001.gd
+++ hit_indicator_0001.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
 
+var rng = RandomNumberGenerator.new()
+
+
+func _ready():
+	transform.origin += Vector2(rng.randf_range(-3, 3), rng.randf_range(-3, 3))
+
+
 func _on_timer_timeout():
 	queue_free()
