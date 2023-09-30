bda4ed698c200495c9fc8e0771b819a4473d359d
indi <indivicivet@gmail.com>
2023-09-30 20:08:13 +0100

    2d moveable player

--- player.gd
+++ player.gd
@@ -7,10 +7,15 @@ const SPEED = 300.0
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x = Input.get_axis("left", "right")
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction_y = Input.get_axis("up", "down")
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
