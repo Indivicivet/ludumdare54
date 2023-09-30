1bdb5046e846d5523c6a7b004ade5c9f64648ca5
indi <indivicivet@gmail.com>
2023-09-30 22:40:37 +0100

    hitsound (but only plays one at once?)

--- scripts/bullet_type1.gd
+++ scripts/bullet_type1.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 
+var hitsound = preload("res://sfx/hitHurt0001.wav")
+
+@onready var audiostream_obj = get_tree().get_root().get_node("game_root").get_node("AudioStreamPlayer2D")
+
+
 const SPEED = 500.0
 var dir = Vector2(1, 0)
 
@@ -11,6 +16,8 @@ func _physics_process(delta):
 
 func _on_body_entered(body):
 	if body.has_method("got_shot"):
+		audiostream_obj.stream = hitsound
+		audiostream_obj.play()
 		body.got_shot(self)
 	if not body.name == "player":
 		queue_free()
