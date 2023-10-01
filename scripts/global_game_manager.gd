4ce23cc4d4b71951ce754223973b24a988062bed
indi <indivicivet@gmail.com>
2023-10-01 22:00:55 +0100

    add mute option

--- scripts/global_game_manager.gd
+++ scripts/global_game_manager.gd
@@ -8,6 +8,12 @@ func _process(_delta):
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
 		return
+	if Input.is_action_just_pressed("mute"):
+		var bgm = get_node("bgm")
+		if bgm.playing:
+			bgm.stop()
+		else:
+			bgm.play()
 	if Input.is_action_just_pressed("restart"):
 		var pos = get_node("play_region").transform.get_origin();
 		get_node("play_region").free()
