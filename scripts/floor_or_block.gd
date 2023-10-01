d85673892ca0007a2479235f80a99c2ed4f71e51
indi <indivicivet@gmail.com>
2023-10-01 13:53:01 +0100

    add sfx for blocks spawning or disappearing

--- scripts/floor_or_block.gd
+++ scripts/floor_or_block.gd
@@ -3,8 +3,12 @@ extends StaticBody2D
 var sprite_solid = preload("res://graphics/tex1_0001__P2000281.png")
 var sprite_floor = preload("res://graphics/tex3_0001__P2000288.png")
 
+var sfx_solidify = preload("res://sfx/tilespawn0001.wav")
+var sfx_liquify = preload("res://sfx/tilebreak0001.wav")
+
 @onready var sprite_obj = get_node("tilesprite")
 @onready var collision_obj = get_node("CollisionShape2D")
+@onready var audio_obj = get_node("AudioStreamPlayer2D")
 
 var rng = RandomNumberGenerator.new()
 
@@ -16,19 +20,25 @@ var is_solid = false
 func liquify():
 	healing = 0
 	charring = 0
-	sprite_obj.texture = sprite_floor
-	sprite_obj.modulate = Color(1, 1, 1)
 	collision_obj.set_deferred("disabled", true)
 	is_solid = false
+	# visual/etc updates:
+	sprite_obj.texture = sprite_floor
+	sprite_obj.modulate = Color(1, 1, 1)
+	audio_obj.stream = sfx_liquify
+	audio_obj.play()
 
 
 func solidify():
 	healing = 0
 	charring = 0
-	sprite_obj.texture = sprite_solid
-	sprite_obj.modulate = Color(1, 1, 1)
 	collision_obj.set_deferred("disabled", false)
 	is_solid = true
+	# visual/etc updates:
+	sprite_obj.texture = sprite_solid
+	sprite_obj.modulate = Color(1, 1, 1)
+	audio_obj.stream = sfx_solidify
+	audio_obj.play()
 
 
 func heal(amt):
