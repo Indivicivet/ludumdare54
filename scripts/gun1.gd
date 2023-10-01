7f78cdb1e662b371e0e28f6abf007a185dee7a17
indi <indivicivet@gmail.com>
2023-10-01 20:57:12 +0100

    refactor gun 1 so doesn't shoot every frame... but it still does cuz firerate is high :)

--- scripts/gun1.gd
+++ scripts/gun1.gd
@@ -6,19 +6,31 @@ const RANDOM_SPREAD = 0.25;
 var rng = RandomNumberGenerator.new()
 var bullet_type_1 = preload("res://bullet_type_1.tscn")
 
+const FIRE_DELAY = 0.03  # probs every frame if fast enough :))
+var fire_next_delay = 0
 
-func _process(_delta):
+
+func fire_bullet():
+	var bullet_inst = bullet_type_1.instantiate()
+	owner.add_child(bullet_inst)
+	bullet_inst.global_transform = self.global_transform
+	var target_dir = (
+		get_local_mouse_position()
+		- self.transform.get_origin()
+	)
+	bullet_inst.dir = (
+		target_dir
+		+ Vector2(
+			rng.randf_range(-1, 1), rng.randf_range(-1, 1)
+		) * target_dir.abs() * RANDOM_SPREAD / 2
+	)
+
+
+func _process(delta):
+	fire_next_delay -= delta
 	if Input.is_action_pressed("primary_fire"):
-		var bullet_inst = bullet_type_1.instantiate()
-		owner.add_child(bullet_inst)
-		bullet_inst.global_transform = self.global_transform
-		var target_dir = (
-			get_local_mouse_position()
-			- self.transform.get_origin()
-		)
-		bullet_inst.dir = (
-			target_dir
-			+ Vector2(
-				rng.randf_range(-1, 1), rng.randf_range(-1, 1)
-			) * target_dir.abs() * RANDOM_SPREAD / 2
-		)
+		if fire_next_delay <= 0:
+			fire_bullet()
+			fire_next_delay = FIRE_DELAY
+		
+		
