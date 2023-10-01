10fda541ce828503f6595f350ca443489079a2bd
indi <indivicivet@gmail.com>
2023-10-01 14:28:28 +0100

    move things into play region -- needed to update gun script

--- scripts/gun1.gd
+++ scripts/gun1.gd
@@ -11,10 +11,10 @@ func _process(_delta):
 	if Input.is_action_pressed("primary_fire"):
 		var bullet_inst = bullet_type_1.instantiate()
 		owner.add_child(bullet_inst)
-		bullet_inst.transform = self.global_transform
+		bullet_inst.global_transform = self.global_transform
 		bullet_inst.dir = (
-			get_viewport().get_mouse_position()
-			- self.global_transform.get_origin()
+			get_local_mouse_position()
+			- self.transform.get_origin()
 			+ Vector2(
 				rng.randf_range(-1, 1), rng.randf_range(-1, 1)
 			) * RANDOM_SPREAD / 2
