2069c8e325d1287aeb6b5d3e48a0aef0dae9e2e9
indi <indivicivet@gmail.com>
2023-10-01 22:23:11 +0100

    health indicator left-based rather than centered (was on the fence but think this is better)

--- scripts/health_indicator.gd
+++ scripts/health_indicator.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 
 func _process(_delta):
+	var bar_size = owner.get_node("play_region/player").health * 400
 	get_node("Sprite2D").set_region_rect(Rect2(
 		0, 0,
-		400 * owner.get_node("play_region/player").health,
+		bar_size,
 		50,
 	))
+	get_node("Sprite2D").position.x = bar_size / 2 - 200
