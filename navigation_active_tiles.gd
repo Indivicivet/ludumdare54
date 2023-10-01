a11bfec7ca7a907926e030e85d43000029ab6851
indi <indivicivet@gmail.com>
2023-10-01 19:23:21 +0100

    convert navigation to use an array of tiles; should allow disabling...

---
+++ navigation_active_tiles.gd
@@ -0,0 +1,32 @@
+extends NavigationRegion2D
+
+
+const TILE_SIZE = 64
+const TILES_N = 6
+const TILES_M = 6
+
+
+func _ready():
+	var polygon = NavigationPolygon.new()
+	var vertices = PackedVector2Array([])
+	for j in range(TILES_M + 1):
+		for i in range(TILES_N + 1):
+			vertices.append(Vector2(i * TILE_SIZE, j * TILE_SIZE))
+	polygon.vertices = vertices
+	for j in range(TILES_M):
+		for i in range(TILES_N):
+			# sanity check: consider the case TILES_N = 1
+			var first_idx = j * (TILES_N + 1) + i
+			polygon.add_polygon(
+				PackedInt32Array([
+					first_idx,
+					first_idx + 1,
+					first_idx + TILES_N + 2,
+					first_idx + TILES_N + 1,
+				])
+			)
+	navigation_polygon = polygon
+
+
+func _process(delta):
+	pass
