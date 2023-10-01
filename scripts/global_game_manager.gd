17cc1df0779e85c4d9a3f0a6b8d1e57f7e7ea3eb
indi <indivicivet@gmail.com>
2023-10-01 17:25:31 +0100

    not-yet-working reset mechanism

--- scripts/global_game_manager.gd
+++ scripts/global_game_manager.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
 
+@onready var play_region = preload("res://play_region.tscn")
+
+
 func _process(_delta):
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
 		return
 	if Input.is_action_just_pressed("restart"):
-		print("restart...")
+		var pos = get_node("play_region").transform.get_origin();
+		get_node("play_region").queue_free()
+		var inst = play_region.instantiate()
+		add_child(inst)
+		inst.transform.origin = pos;
