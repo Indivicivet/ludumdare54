7781b8709a3098cac723643ecdcc7b00721d7c56
indi <indivicivet@gmail.com>
2023-10-01 18:58:38 +0100

    make random spread not aim pos dependent and increase it somewhat

--- scripts/gun1.gd
+++ scripts/gun1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-const RANDOM_SPREAD = 3;
+const RANDOM_SPREAD = 0.25;
 
 var rng = RandomNumberGenerator.new()
 var bullet_type_1 = preload("res://bullet_type_1.tscn")
@@ -12,10 +12,13 @@ func _process(_delta):
 		var bullet_inst = bullet_type_1.instantiate()
 		owner.add_child(bullet_inst)
 		bullet_inst.global_transform = self.global_transform
-		bullet_inst.dir = (
+		var target_dir = (
 			get_local_mouse_position()
 			- self.transform.get_origin()
+		)
+		bullet_inst.dir = (
+			target_dir
 			+ Vector2(
 				rng.randf_range(-1, 1), rng.randf_range(-1, 1)
-			) * RANDOM_SPREAD / 2
+			) * target_dir.abs() * RANDOM_SPREAD / 2
 		)
