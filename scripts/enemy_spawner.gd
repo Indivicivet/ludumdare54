5d4832d34156976f42f67ca3f93144ea5d3a03f0
indi <indivicivet@gmail.com>
2023-10-01 20:43:50 +0100

    add second enemy type (bigger, basically)

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 
+var rng = RandomNumberGenerator.new()
+
 var enemy_type1_scn = preload("res://enemy_type_1.tscn")
+var enemy_type2_scn = preload("res://enemy_type_2.tscn")
 
 
 func _on_timer_timeout():
-	var enemy_obj = enemy_type1_scn.instantiate()
+	var enemy_obj = (
+		enemy_type1_scn.instantiate()
+		if rng.randf() < 0.75
+		else enemy_type2_scn.instantiate()
+	)
 	add_child(enemy_obj)
 	enemy_obj.transform = Transform2D.IDENTITY
 	get_node("AudioStreamPlayer2D").play()
---
+++ scripts/enemy_type2.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+const SPEED = 50  # CHANGED VS TYPE 1
+
+var target_pos = Vector2(0, 0)
+var hp = 1
+
+
+@onready var navigation_agent = get_node("NavigationAgent2D")
+@onready var sprite_obj = get_node("Sprite2D")
+@onready var root = get_tree().get_root().get_node("game_root")
+@onready var player = root.get_node("play_region/player")
+@onready var score = root.get_node("score")
+
+
+func _ready():
+	sprite_obj.modulate = Color(1, 1, 1)
+	navigation_agent.path_desired_distance = 1.0
+	navigation_agent.target_desired_distance = 1.0
+	call_deferred("pathfinding_setup")
+
+
+func got_shot(_other):
+	hp -= 0.01  # CHANGED VS TYPE 1
+	sprite_obj.modulate = Color(hp, hp, hp)
+	if hp < 0:
+		score.score += 1
+		queue_free()
+
+
+func pathfinding_setup():
+	await get_tree().physics_frame
+	navigation_agent.target_position = target_pos
+
+
+func _physics_process(delta):
+	navigation_agent.target_position = player.global_position
+	if navigation_agent.is_navigation_finished():
+		return
+
+	velocity = (
+		navigation_agent.get_next_path_position() - global_position
+	).normalized() * SPEED
+	move_and_slide()
+	for i in get_slide_collision_count():
+		var collider = get_slide_collision(i).get_collider()
+		if collider.name == "player":
+			collider.touched_by_enemy(self, delta)
